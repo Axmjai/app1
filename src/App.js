441e093ff3f2eedb7dcbe842b4497868a654d4b7
Axmjai <152575142+Axmjai@users.noreply.github.com>
2025-01-08 15:05:00 +0700

    add calculator.js
    add calculator.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import Button from './class-components';
+import { Calculator2 } from './Calculate'
 
-  function App() {
-    return <Button/>  
+
+ function App() {
+    return <Calculator2/>  
   }
 
-export default App;
+  export default App
